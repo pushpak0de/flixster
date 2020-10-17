a3022fdba107c0c5efe363889e840265cef43d19
Pushpak Patel <pushpakpatel123@gmail.com>
2020-10-17 15:24:04 -0700

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Pushpak Patel on 10/17/20.
+//  Copyright © 2020 Pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
