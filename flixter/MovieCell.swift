4d249a941a971f7042497b5401eeafcb4b3880db
Pushpak Patel <pushpakpatel123@gmail.com>
2020-10-20 17:07:21 -0700

    features

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Pushpak Patel on 10/19/20.
+//  Copyright © 2020 Pak. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
