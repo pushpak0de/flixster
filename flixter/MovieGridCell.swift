41ace684590c309972251fbff7ce6026dad1c97f
Pushpak Patel <pushpakpatel123@gmail.com>
2020-10-24 15:11:21 -0700

    New features: tab bar and collection view

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Pushpak Patel on 10/24/20.
+//  Copyright © 2020 Pak. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
